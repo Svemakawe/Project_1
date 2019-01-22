d68308c20f2be58bfbb68ad5d9202eee201edcab
Svenja Weiler <Svenja.Weiler@gmx.de>
2019-01-22 15:27:56 +0100

    game ends when hitting left

--- lemmingEscape/js/main.js
+++ lemmingEscape/js/main.js
@@ -3,20 +3,21 @@ var myObstacles = [];
 
 function startGame() {
     myGameArea.start();
-    player = new component(30, 30, "#472b06", 0, 110);//---------NEW PLAYER--------
+    player = new component(50, 50, "#472b06", 30, 110);//---------NEW PLAYER--------
+    // looseLine = new component(12,1,"blue",20,770)
 }
 
 //--------------------------------GAME AREA-----------------------------
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 980;//480
+        this.canvas.width = 780;//480
         this.canvas.height = 770;//270
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
       this.interval = setInterval(updateGameArea, 20);
     },
-    frames: 0,
+    frames: 0,//0
     clear : function() {
           this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
       },
@@ -29,6 +30,7 @@ var myGameArea = {
   stop : function() {
         clearInterval(this.interval);
     }
+  
 }
 
 //----------------------------COMPONENTS CLASS-----------------------------
@@ -48,43 +50,62 @@ function component(width, height, color, x, y) {
         this.x += this.speedX;
         this.y += this.speedY; 
     }
+   
     this.left   = function() { return this.x                 }
     this.right  = function() { return (this.x + this.width)  }
     this.top    = function() { return this.y                 }
     this.bottom = function() { return this.y + (this.height) }
+    //testcrash
+    // this.crash = function(obstacle){ return this.speedX = obstacle.speedX}
 
 //---------------------------CRASH IN OBSTACLE--------------------------------
-    // this.crashWith = function(obstacle) {
-    //   return !((this.bottom() < obstacle.top())    ||
-    //            (this.top()    > obstacle.bottom()) ||
-    //            (this.right()  < obstacle.left())   ||
-    //            (this.left()   > obstacle.right())) 
-    // }
+    this.crashWith = function(obstacle) {
+        return !((this.bottom() < obstacle.top())    ||
+                 (this.top()    > obstacle.bottom()) ||
+                 (this.right()  < obstacle.left())   ||
+                 (this.left()   > obstacle.right())) 
+      }
 }
 
 //-------------------------UPDATING THE CANVAS---------------------------------
 function updateGameArea() {
-    // for (i = 0; i < myObstacles.length; i += 1) {
-    //     if (player.crashWith(myObstacles[i])) {
-    //         myGameArea.stop();
-    //         return;
-    //     } 
-    // } //---------------------STOP PLAYER WHEN CRASHING---------------------
+    //-------------------PLAYER CANT MOVE WHEN CRASHING---------------------
+
+    for (i = 0; i < myObstacles.length; i += 1) {
+        if (player.crashWith(myObstacles[i])) {
+             player.x = myObstacles[i].x
+        } 
+    } 
+    //-------------PLAYER CANT MOVE WHEN CRASHING THE END OF CANVAS--------------
+    if (player.x > 780){
+        player.speedX = 0;
+    }
+
+
+    //---------------------STOP WHEN LOOSING---------------------
+    if (player.x < 0) {
+        myGameArea.stop();
+      }
+
+
     myGameArea.clear();
-    myGameArea.frames +=1;//---------------------CREATING OBSTACLES-----------
+    myGameArea.frames +=1;//1-4
+
+    //---------------------CREATING OBSTACLES-----------------------
     if (myGameArea.frames % 100 === 0) {
         x = myGameArea.canvas.width;
-        minHeight = 20;
-        maxHeight = 200;
+        minHeight = 20;//20
+        maxHeight = 200;//200
         height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
-        minGap = 50;
-        maxGap = 200;
+        minGap = 70;//50
+        maxGap = 260;//200
+
         gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
-        myObstacles.push(new component(10, height, "#a76106", x, 0));
-        myObstacles.push(new component(10, x - height - gap, "#a76106", x, height + gap));
+        myObstacles.push(new component(30, height, "#a76106", x, 0));//
+        myObstacles.push(new component(30, x - height - gap, "#a76106", x, height + gap));
     }
     for (i = 0; i < myObstacles.length; i += 1) {
-        myObstacles[i].x += -8;//macht es schneller
+        myObstacles[i].x += -8;//-1 macht es schneller
         myObstacles[i].update();
     }
     player.newPos();
@@ -94,19 +115,19 @@ function updateGameArea() {
 
 //---------------------------MOVE THE PLAYER --------------------------------
 function moveUp() {
-    player.speedY -= 1; 
+    player.speedY -= 1;//1 
 }
 
 function moveDown() {
-    player.speedY += 1; 
+    player.speedY += 1;//1 
 }
 
 function moveLeft() {
-    player.speedX -= 1;
+    player.speedX -= 1;//1 
 }
 
 function moveRight() {
-    player.speedX += 1;
+    player.speedX += 1;//1 
 }
 
 document.onkeydown = function(e) {
@@ -126,7 +147,7 @@ document.onkeydown = function(e) {
   }
 }
 
-//---------------------------STOP THE PLAYER --------------------------------
+//--------------------------DONT MOVE THE PLAYER -----------------------------
 document.onkeyup = function(e) {
   stopMove();
 }
